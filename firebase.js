c4fd1f2e23d8469e5de20b702884ff1284241ff6
healthifylab <gag92gag92@gmail.com>
2025-06-28 00:08:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
+  apiKey: "AIzaSyDS-MJYzAB2EDNY7Hhy2RtdEkxflj2jI-A",
   authDomain: "healthify-lab.firebaseapp.com",
   projectId: "healthify-lab",
-  storageBucket: "healthify-lab.appspot.com",
-  messagingSenderId: "YOUR-SENDER-ID",
-  appId: "YOUR-APP-ID"
+  storageBucket: "healthify-lab.firebasestorage.app",
+  messagingSenderId: "297003315332",
+  appId: "1:297003315332:web:49f6ed6fc61cce4a74d2d1",
+  measurementId: "G-R0R3RYERZW"
 };
 
 const app = initializeApp(firebaseConfig);
