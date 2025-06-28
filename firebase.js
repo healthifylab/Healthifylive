64d86197e6d5aa6f4c604628dfedfef8d2426f35
healthifylab <gag92gag92@gmail.com>
2025-06-28 23:26:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
-// Firebase connection to gag92gag92@gmail.com
+// firebase.js
+// Make sure to replace with your actual Firebase config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "healthify-lab",
+  storageBucket: "healthify-lab.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
