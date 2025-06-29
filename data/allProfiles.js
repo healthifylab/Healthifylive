9fc0a8627d3eb243bd25d820529b2413a0ef8c60
healthifylab <gag92gag92@gmail.com>
2025-06-29 14:07:13 +0530

    Create allProfiles.js

---
+++ data/allProfiles.js
@@ -0,0 +1,25 @@
+// data/allProfiles.js
+export const allProfiles = [
+  {
+    name: "Basic Health Package",
+    mrp: 1499,
+    discounted: 1199,
+    description: "CBC, LFT, RFT, Lipid, Sugar",
+    tat: "24 Hrs",
+  },
+  {
+    name: "Advance Diabetes Profile",
+    mrp: 999,
+    discounted: 799,
+    description: "Fasting sugar, HbA1c, Lipid panel",
+    tat: "12 Hrs",
+  },
+  {
+    name: "Women Wellness Package",
+    mrp: 1999,
+    discounted: 1599,
+    description: "CBC, Hormonal, Thyroid, Vitamin D",
+    tat: "Same Day",
+  },
+  // Add more from Dr. C, Healthians, GD-lab, etc.
+];
