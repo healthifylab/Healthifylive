263e6e6e8c44252fa35cf65a144ae9c2fac6fd15
healthifylab <gag92gag92@gmail.com>
2025-06-29 21:21:13 +0530

    Update and rename App.jsx to App.js

---
+++ App.js
@@ -0,0 +1,6 @@
+import AdminLogin from "./pages/AdminLogin";
+import AdminPanel from "./pages/AdminPanel";
+
+// Inside your Routes
+<Route path="/admin-login" element={<AdminLogin />} />
+<Route path="/admin" element={<AdminPanel />} />
