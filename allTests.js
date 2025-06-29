3a9e3ddd90be4cd539138cb6ecbf3e5c6d6a50df
healthifylab <gag92gag92@gmail.com>
2025-06-29 16:28:18 +0530

    Update and rename all tests.js to allTests.js

---
+++ allTests.js
@@ -0,0 +1,74 @@
+// routes/allTests.js
+
+export const allTests = [
+  {
+    name: "CBC (Complete Blood Count)",
+    mrp: 300,
+    price: 199,
+    tat: "12 hrs",
+    description: "CBC (Complete Blood Count) test with TAT of 12 hrs"
+  },
+  {
+    name: "LFT (Liver Function Test)",
+    mrp: 600,
+    price: 449,
+    tat: "24 hrs",
+    description: "LFT (Liver Function Test) test with TAT of 24 hrs"
+  },
+  {
+    name: "KFT (Kidney Function Test)",
+    mrp: 700,
+    price: 499,
+    tat: "24 hrs",
+    description: "KFT (Kidney Function Test) test with TAT of 24 hrs"
+  },
+  {
+    name: "HbA1c",
+    mrp: 600,
+    price: 399,
+    tat: "12 hrs",
+    description: "HbA1c test with TAT of 12 hrs"
+  },
+  {
+    name: "Thyroid Profile (T3, T4, TSH)",
+    mrp: 750,
+    price: 549,
+    tat: "24 hrs",
+    description: "Thyroid Profile (T3, T4, TSH) test with TAT of 24 hrs"
+  },
+  {
+    name: "Vitamin B12",
+    mrp: 1100,
+    price: 849,
+    tat: "24 hrs",
+    description: "Vitamin B12 test with TAT of 24 hrs"
+  },
+  {
+    name: "D-Dimer",
+    mrp: 1200,
+    price: 999,
+    tat: "24 hrs",
+    description: "D-Dimer test with TAT of 24 hrs"
+  },
+  {
+    name: "Amylase",
+    mrp: 600,
+    price: 399,
+    tat: "24 hrs",
+    description: "Amylase test with TAT of 24 hrs"
+  },
+  {
+    name: "Ferritin",
+    mrp: 700,
+    price: 499,
+    tat: "24 hrs",
+    description: "Ferritin test with TAT of 24 hrs"
+  },
+  {
+    name: "PT/INR",
+    mrp: 600,
+    price: 449,
+    tat: "24 hrs",
+    description: "PT/INR test with TAT of 24 hrs"
+  }
+];
