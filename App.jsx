bcbd50fc2c6fe4076d2cc5fde6138723664dfc4f
healthifylab <gag92gag92@gmail.com>
2025-06-29 14:45:22 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,27 +1,33 @@
+// App.jsx
 import React from "react";
+import SearchBar from "./components/SearchBar";
 import BookingForm from "./components/BookingForm";
+import AdminPanel from "./components/AdminPanel"; // Optional route
+import FAQ from "./components/FAQ";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import PromoPopup from "./components/PromoPopup";
+
+import { allTests } from "./data/allTests";
 
 function App() {
   return (
-    <div className="pt-20">
+    <div className="bg-gray-50 min-h-screen pt-20">
+      {/* 🎉 Promo Popup */}
+      <PromoPopup />
+
+      {/* 🔍 Direct Search Bar (outside menu) */}
+      <SearchBar allTests={allTests} />
+
+      {/* 🧾 Booking Form */}
       <BookingForm />
+
+      {/* ❓ FAQs */}
+      <FAQ />
+
+      {/* 🟢 Floating WhatsApp */}
+      <FloatingWhatsApp />
     </div>
   );
 }
 
 export default App;
-import BookingForm from "./components/BookingForm";
-import FAQ from "./components/FAQ";
-import FloatingWhatsApp from "./components/FloatingWhatsApp";
-import PromoPopup from "./components/PromoPopup";
-
-function App() {
-  return (
-    <>
-      <PromoPopup />
-      <BookingForm />
-      <FAQ />
-      <FloatingWhatsApp />
-    </>
-  );
-}
