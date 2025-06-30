2a44fd0d3c72c22380b43ab40d5633e254222abc
healthifylab <gag92gag92@gmail.com>
2025-06-30 16:50:26 +0530

    Delete App.js

--- App.js
+++
@@ -1,6 +0,0 @@
-import AdminLogin from "./pages/AdminLogin";
-import AdminPanel from "./pages/AdminPanel";
-
-// Inside your Routes
-<Route path="/admin-login" element={<AdminLogin />} />
-<Route path="/admin" element={<AdminPanel />} />
