900993f9f1446dacb98e380d47ac468c59e26e6f
healthifylab <gag92gag92@gmail.com>
2025-07-03 18:06:05 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,1 +1,12 @@
-// EmailJS integration using provided service/template/public key
+// EmailJS // ✅ EmailJS Integration
+import emailjs from '@emailjs/browser';
+
+export const sendEmail = (templateParams) => {
+  return emailjs.send(
+    'service_z3ac4pk',         // Service ID
+    'template_5v6t6ku',        // Template ID
+    templateParams,
+    'dJE_JHAoNTxxzTxiT'        // Public Key
+  );
+};
+ using provided service/template/public key
