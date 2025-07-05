a6fdccfb04780696e6a6a3c2fd48a553af497b77
healthifylab <gag92gag92@gmail.com>
2025-07-05 23:13:04 +0530

    Create pwa-regster.js

---
+++ public/pwa-regster.js
@@ -0,0 +1,12 @@
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/public/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
