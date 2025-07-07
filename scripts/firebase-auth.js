c41b7c4cd2f7ceb8b404542459e937be526bda44
healthifylab <gag92gag92@gmail.com>
2025-07-08 02:24:24 +0530

    Update firebase-auth.js

--- scripts/firebase-auth.js
+++ scripts/firebase-auth.js
@@ -1,17 +1,21 @@
-// scripts/Firebase-auth.js
+// scripts/Firebase-auth.js (Optional, if kept separate)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth, signInWithPhoneNumber, RecaptchaVerifier, sendSignInLinkToEmail } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPhoneNumber, RecaptchaVerifier, sendSignInLinkToEmail, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDS-MJYzAB2EDNY7Hhy2RtdEkxflj2jI-A",
     authDomain: "healthify-lab.firebaseapp.com",
+    databaseURL: "https://healthify-lab-default-rtdb.firebaseio.com",
     projectId: "healthify-lab",
     storageBucket: "healthify-lab.firebasestorage.app",
     messagingSenderId: "297003315332",
-    appId: "1:297003315332:web:49f6ed6fc61cce4a74d2d1"
+    appId: "1:297003315332:web:49f6ed6fc61cce4a74d2d1",
+    measurementId: "G-R0R3RYERZW"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 window.recaptchaVerifier = new RecaptchaVerifier("recaptcha-container", {
@@ -24,7 +28,7 @@ export function sendOTP(phoneNumber) {
 
 export function sendEmailLink(email) {
     const actionCodeSettings = {
-        url: "https://healthifylive.vercel.app/booking.html",
+        url: "https://healthifylive.vercel.app/index.html",
         handleCodeInApp: true
     };
     return sendSignInLinkToEmail(auth, email, actionCodeSettings);
