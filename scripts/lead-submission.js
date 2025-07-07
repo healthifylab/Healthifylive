9d1b69900beee9a99bd8e6a092485fca0924be78
healthifylab <gag92gag92@gmail.com>
2025-07-08 02:30:56 +0530

    Update lead-submission.js

--- scripts/lead-submission.js
+++ scripts/lead-submission.js
@@ -5,10 +5,12 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 const firebaseConfig = {
     apiKey: "AIzaSyDS-MJYzAB2EDNY7Hhy2RtdEkxflj2jI-A",
     authDomain: "healthify-lab.firebaseapp.com",
+    databaseURL: "https://healthify-lab-default-rtdb.firebaseio.com",
     projectId: "healthify-lab",
     storageBucket: "healthify-lab.firebasestorage.app",
     messagingSenderId: "297003315332",
-    appId: "1:297003315332:web:49f6ed6fc61cce4a74d2d1"
+    appId: "1:297003315332:web:49f6ed6fc61cce4a74d2d1",
+    measurementId: "G-R0R3RYERZW"
 };
 
 const app = initializeApp(firebaseConfig);
